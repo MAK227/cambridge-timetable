aa92f3adbdda8dc45f2d8d504b13f55d6ccdef33
MAK227 <assadkhan@windowslive.com>
2024-01-18 23:35:47 +0500

    attempt 3

--- pages/_app.js
+++ pages/_app.js
@@ -7,11 +7,13 @@ import { Fragment } from "react";
 function MyApp({ Component, pageProps }) {
   return (
     <Fragment>
-      <NextNProgress color="#00ff85" />
-      <div className="min-h-screen w-screen overflow-x-hidden bg-dark font-poppins text-white">
-        <Component {...pageProps} />
-      </div>
-      <SpeedInsights />
+      <Fragment>
+        <NextNProgress color="#00ff85" />
+        <div className="min-h-screen w-screen overflow-x-hidden bg-dark font-poppins text-white">
+          <Component {...pageProps} />
+        </div>
+        <SpeedInsights />
+      </Fragment>
       <Analytics />
     </Fragment>
   );
