9d9cea45923901d75f273bfc62518ec5138e71db
MAK227 <assadkhan@windowslive.com>
2024-01-18 23:37:31 +0500

    roll back to only analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,15 @@
 import "@/styles/globals.css";
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from "@vercel/speed-insights/next";
 import NextNProgress from "nextjs-progressbar";
 import { Fragment } from "react";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Fragment>
-      <Fragment>
-        <NextNProgress color="#00ff85" />
-        <div className="min-h-screen w-screen overflow-x-hidden bg-dark font-poppins text-white">
-          <Component {...pageProps} />
-        </div>
-        <SpeedInsights />
-      </Fragment>
+      <NextNProgress color="#00ff85" />
+      <div className="min-h-screen w-screen overflow-x-hidden bg-dark font-poppins text-white">
+        <Component {...pageProps} />
+      </div>
       <Analytics />
     </Fragment>
   );
